3b41e0f294a71c0d4b98c8badeb4bb7916d16e81
unknown <ruchahuja1228@gmail.com>
2020-10-25 19:25:55 +0530

    Updates

---
+++ precache-manifest.0e42d133a6ead64c17eede9077c5ddb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a920e41f8229fdc8526d975d27577f",
+    "url": "/netflix_clone/index.html"
+  },
+  {
+    "revision": "84f8dac62e279ba69add",
+    "url": "/netflix_clone/static/css/main.bf134908.chunk.css"
+  },
+  {
+    "revision": "093cec8f41bda5a0b609",
+    "url": "/netflix_clone/static/js/2.3cd73aca.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix_clone/static/js/2.3cd73aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f8dac62e279ba69add",
+    "url": "/netflix_clone/static/js/main.2d53439f.chunk.js"
+  },
+  {
+    "revision": "7dcf5a99bbe09cfa90a7",
+    "url": "/netflix_clone/static/js/runtime-main.017780d1.js"
+  }
+]);
